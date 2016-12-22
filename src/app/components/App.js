e024973fd5ad176fe85b7386227acfadf2d2f5f0
Svartingknas <svartingknas@gmail.com>
2016-12-22 01:22:09 -0700

    chore(feat)remove components home and Entries

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,20 +1,48 @@
-"use strict";
-
 import React from 'react';
+import ReactDom from 'react-dom'
 import Sidebar from './Sidebar.js'
 import Header from './Header.js'
-import Entries from './Entries.js'
 import Footer from './Footer.js'
-import Home from './Home.js'
 import * as firebase from 'firebase'
 
+var config = {
+  apiKey: "AIzaSyDWsAEg983OYIsK2cnPpv6qC9kWUjFpM2E",
+  authDomain: "svartingknas-1296.firebaseapp.com",
+  databaseURL: "https://svartingknas-1296.firebaseio.com",
+  storageBucket: "svartingknas-1296.appspot.com",
+  messagingSenderId: "449322754463"
+}
+firebase.initializeApp(config);
+
 export default class App extends React.Component {
+  constructor() {
+  super()
+  this.state = {
+    name: "sergio morales"
+  }
+}
+
+componentWillMount () {
+  const nameRef = firebase.database().ref().child('object').child('name')
+
+  nameRef.on('value', (snapshot) => {
+    this.setState({
+      name: snapshot.val()
+    })
+  })
+}
+
   render () {
     return (
       <div className='container'>
         <Header />
         <Sidebar />
-        <Entries  content={this.props.nameRef}/>
+          <div className="container">
+            <div className="row">
+              <h2>Blog Entries</h2>
+               {this.state.name}
+              </div>
+            </div>
         <Footer />
       </div>
     );
